c6fe4166732bedc6e8fdcc976b4619ce2f3d5b45
sip615 <sunilpatel.328@gmail.com>
2019-05-12 16:34:38 -0400

    committing server.js

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-var express = require("express");
+const express = require("express");
+
+const PORT = process.env.PORT || 3306;
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const expressHandlebars = require("express-handlebars");
+
+app.engine("handlebars", expressHandlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/burgers_controller");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+	console.log("Listening on port:%s", PORT);
+});
